a43330b5527a009b1cf68f49644f1377e29d027b
Nick Veenhof <nick.veenhof@gmail.com>
2016-11-02 15:25:49 +0100

    renamed test to Test

--- test/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$autoloadFile = __DIR__.'/../vendor/autoload.php';
-if (!file_exists($autoloadFile)) {
-    throw new RuntimeException('Install dependencies to run phpunit.');
-}
-$loader = require $autoloadFile;
