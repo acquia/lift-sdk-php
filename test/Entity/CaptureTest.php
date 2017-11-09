01a357dd0be1371d42ef65068031f368f1463692
Matthew Morley <matthew.morley@acquia.com>
2017-11-09 09:56:45 -0500

    LAT-1144 | Update DateTime format to 3 digits (#24)
    * LAT-1144 | Update DateTime format to 3 digits
    
    * LAT-1144 | Update related tests to pass in travis

--- test/Entity/CaptureTest.php
+++ test/Entity/CaptureTest.php
@@ -157,7 +157,7 @@ class CaptureTest extends \PHPUnit_Framework_TestCase
         $date = DateTime::createFromFormat(DateTime::ATOM, '2016-01-05T22:04:39Z');
         $entity = new Capture();
         $entity->setEventDate($date);
-        $this->assertEquals($entity->json(), '{"event_date":"2016-01-05T22:04:39.000000Z"}');
+        $this->assertEquals($entity->json(), '{"event_date":"2016-01-05T22:04:39.000Z"}');
     }
 
     public function testIdentities()
@@ -768,6 +768,6 @@ class CaptureTest extends \PHPUnit_Framework_TestCase
         $date = DateTime::createFromFormat(DateTime::ATOM, '2016-01-05T22:04:39Z');
         $entity = new Capture();
         $entity->setPublishedDate($date);
-        $this->assertEquals($entity->json(), '{"published_date":"2016-01-05T22:04:39.000000Z"}');
+        $this->assertEquals($entity->json(), '{"published_date":"2016-01-05T22:04:39.000Z"}');
     }
 }
