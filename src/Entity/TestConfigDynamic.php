3c3a3e8e0fe0dfae1fd026b28f8eb762cbe83bc8
Bao Phan <baophan1@gmail.com>
2020-01-09 15:57:06 -0500

    Add remaining fields for dynamic
    LEB-3141

--- src/Entity/TestConfigDynamic.php
+++ src/Entity/TestConfigDynamic.php
@@ -62,7 +62,7 @@ class TestConfigDynamic extends TestConfigBase implements TestConfigInterface
         return $this->getEntityValue('filter_id', '');
     }
 
-/**
+    /**
      * Sets the 'algorithm' parameter.
      *
      * @param string $algorithm 
@@ -84,11 +84,98 @@ class TestConfigDynamic extends TestConfigBase implements TestConfigInterface
     /**
      * Gets the 'algorithm' parameter.
      *
-     * @return string The Filter Id associated with dynamic rule
+     * @return string Algorithm 
      */
     public function getAlgorithm()
     {
-        return $this->getEntityValue('filter_id', '');
+        return $this->getEntityValue('algorithm', '');
+    }
+
+    /**
+     * Sets the 'view_mode_id' parameter.
+     *
+     * @param string $viewModeId 
+     *
+     * @throws \Acquia\LiftClient\Exception\LiftSdkException
+     *
+     * @return \Acquia\LiftClient\Entity\TestConfigDynamic
+     */
+    public function setViewModeId($viewModeId)
+    {
+        if (!is_string($viewModeId)) {
+            throw new LiftSdkException('Argument must be an instance of string.');
+        }
+        $this['view_mode_id'] = $viewModeId;
+
+        return $this;
+    }
+
+    /**
+     * Gets the 'view_mode_id' parameter.
+     *
+     * @return string Algorithm 
+     */
+    public function getViewModeId()
+    {
+        return $this->getEntityValue('view_mode_id', '');
+    }
+
+    /**
+     * Sets the 'count' parameter.
+     *
+     * @param int $count 
+     *
+     * @throws \Acquia\LiftClient\Exception\LiftSdkException
+     *
+     * @return \Acquia\LiftClient\Entity\TestConfigDynamic
+     */
+    public function setCount($count)
+    {
+        if (!is_int($count) && $count >= 0) {
+            throw new LiftSdkException('Argument must be an instance of integer.');
+        }
+        $this['count'] = $viewModeId;
+
+        return $this;
+    }
+
+    /**
+     * Gets the 'count' parameter.
+     *
+     * @return int count 
+     */
+    public function getCount()
+    {
+        return $this->getEntityValue('count', '');
+    }
+
+    /**
+     * Sets the 'exclude_viewed_content' parameter.
+     *
+     * @param bool $excludeViewedContent Flag to indicate to exclude viewed content. Only return results with 'view' value of 0
+     *
+     * @throws \Acquia\LiftClient\Exception\LiftSdkException
+     *
+     * @return \Acquia\LiftClient\Entity\Decide
+     */
+    public function setExcludeViewedContent($excludeViewedContent)
+    {
+        if (!is_bool($excludeViewedContent)) {
+            throw new LiftSdkException('Argument must be an instance of boolean.');
+        }
+        $this['exclude_viewed_content'] = $excludeViewedContent;
+
+        return $this;
+    }
+
+    /**
+     * Gets the 'exclude_viewed_content' parameter.
+     *
+     * @return boolean  
+     */
+    public function getExcludeViewedContent()
+    {
+        return $this->getEntityValue('exclude_viewed_content', '');
     }
 
     /**
